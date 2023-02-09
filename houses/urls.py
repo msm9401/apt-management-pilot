201e96b693bccd2db3dcbac2528876b1a00a4cd5
msm9401 <anstmdals94@gmail.com>
2023-02-09 18:29:30 +0900

    Modify the home screen based on whether you are logged in or not

--- houses/urls.py
+++ houses/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
-    path("", views.ApartmentRandomList.as_view()),  # 홈화면
-    path("", views.SearchApartment.as_view()),
-    path("<str:kapt_name>", views.MyApartment.as_view()),
+    path("", views.ApartmentList.as_view()),
+    path("search", views.SearchApartment.as_view()),
 ]
--- houses/views.py
+++ houses/views.py
@@ -4,23 +4,24 @@ from django.db.models import Max, Min
 from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework.exceptions import ParseError, NotFound
-
-# from rest_framework import generics
-# from rest_framework import filters
+from rest_framework.exceptions import ParseError
 
 from .serializers import ApartmentSerializer
 from .models import Apartment
 
 
-class ApartmentRandomList(APIView):
+class ApartmentList(APIView):
 
     """
-    랜덤 아파트 리스트
+    랜덤 아파트 리스트(로그인 안했을때)
+    본인 아파트(로그인 했을때)
     """
 
     def get(self, request):
+        if not request.user.is_anonymous:
+            my_houses = request.user.my_houses
+            serializer = ApartmentSerializer(my_houses, many=True)
+            return Response(serializer.data)
         max_apt_id = Apartment.objects.aggregate(max_apt_id=Max("id"))["max_apt_id"]
         min_apt_id = Apartment.objects.aggregate(min_apt_id=Min("id"))["min_apt_id"]
         random_apt_list = []
@@ -38,47 +39,21 @@ class ApartmentRandomList(APIView):
 class SearchApartment(APIView):
 
     """
-    특정 아파트 검색(아파트 이름으로 검색)
+    특정 아파트 검색(아파트 이름으로 검색 추천)
     """
 
     def get(self, request):
-        try:
-            text = request.query_params.get("search", "")
-            text = str(text)
-        except:
-            text = ""
-        if text != "":
-            apt_searched = Apartment.objects.filter(kapt_name__contains=text)
-        else:
+        if not request.query_params.get("keyword"):
             raise ParseError("검색어를 입력하세요.")
+        if len(request.query_params.get("keyword")) == 1:
+            raise ParseError("1글자 이상으로 검색하세요.")
+        apt_searched = Apartment.objects.filter(
+            Q(kapt_name__icontains=request.query_params.get("keyword"))
+            | Q(address_do__icontains=request.query_params.get("keyword"))
+            | Q(address_si__icontains=request.query_params.get("keyword"))
+            | Q(address_dong__icontains=request.query_params.get("keyword"))
+        )
+        if not apt_searched:
+            raise ParseError("찾으시는 결과가 없습니다.")
         serializers = ApartmentSerializer(apt_searched, many=True)
         return Response(serializers.data)
-
-
-""" class SearchApartment(generics.ListAPIView):
-
-    queryset = Apartment.objects.all()
-    serialzer_class = ApartmentSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = [
-        "address_do",
-        "address_si",
-        "address_dong",
-        "address_li",
-        "kapt_name",
-    ] """
-
-
-class MyApartment(APIView):
-
-    """
-    내 아파트
-    """
-
-    def get(self, request, kapt_name):
-        try:
-            my_apt = Apartment.objects.get(kapt_name=kapt_name)
-        except Apartment.DoesNotExist:
-            raise NotFound
-        serializer = ApartmentSerializer(my_apt)
-        return Response(serializer.data)
