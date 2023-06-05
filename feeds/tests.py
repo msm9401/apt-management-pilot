fcba776d86cc4147fbb74ddeef1f2e2a3c04fb19
msm9401 <anstmdals94@gmail.com>
2023-06-05 19:26:28 +0900

    Fix : 피트 테스트 수정
    - 피드 post에 본인의 아파트를 검증하는 로직이 추가되어 테스트코드 실패로 인한 수정

--- feeds/tests.py
+++ feeds/tests.py
@@ -52,40 +52,45 @@ class FeedTest(APITestCase):
         )
 
     def test_get_feed_list(self):
-        """
-        유저의 아파트랑 url의 아파트이름이 같지않거나 유저가 아파트가 존재하지 않을때 접근금지 테스트
-        유저의 아파트가 url의 아파트이름이 같으면 접근가능 테스트
-        """
+        """피드 불러오기 테스트"""
 
-        response = self.client.get(
-            self.url,
-            HTTP_AUTHORIZATION=f"token {self.token}",
-        )  # 접근불가능한지 의도적으로 오류
-        print("'Forbidden' is the intended message.")
-        self.assertEqual(response.status_code, 403)  # 유저의 집이 저장 안되어있을때 403오류
-
-        self.user.my_houses.add(self.apt)  # 유저의 집 저장
-        self.user.save()
+        # 유저 아파트 설정 전 접근
         response = self.client.get(
             self.url,
             HTTP_AUTHORIZATION=f"token {self.token}",
         )
-        self.assertEqual(response.status_code, 200)  # 유저의 집 저장 되어있을때
+        self.assertEqual(response.status_code, 403)
+
+        # 유저 아파트 설정
+        self.user.my_houses.add(self.apt)
+        self.user.save()
+
+        # 본인 아파트의 feed에 성공적으로 접근
+        response = self.client.get(
+            self.url,
+            HTTP_AUTHORIZATION=f"token {self.token}",
+        )
+        self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.data, list)
         self.assertEqual(response.data[0]["content"], self.feed.content)
         self.assertEqual(response.data[0]["house"], self.feed.house.id)
         self.assertEqual(response.data[0]["user"]["username"], self.feed.user.username)
 
     def test_create_feed(self):
+        """피드 생성 테스트"""
+
+        # 유저 아파트 설정 전 접근
         response = self.client.post(
             self.url,
             HTTP_AUTHORIZATION=f"token {self.token}",
-        )  # 의도적인 오류
-        print("'Bad Request' is the intended message.")
-        self.assertEqual(response.status_code, 400)
+        )
+        self.assertEqual(response.status_code, 403)
 
+        # 유저 아파트 설정
         self.user.my_houses.add(self.apt)
         self.user.save()
+
+        # 본인 아파트에 성공적으로 feed생성
         response = self.client.post(
             path=self.url,
             data=self.feed_data,
@@ -93,25 +98,36 @@ class FeedTest(APITestCase):
         )
         self.assertEqual(response.status_code, 201)
 
-    def test_get_feed_datial(self):
-        """
-        특정 피드 접근 테스트
-        """
+        # 필수값인 content에 data없이 post
+        response = self.client.post(
+            path=self.url,
+            data={"content": ""},
+            HTTP_AUTHORIZATION=f"token {self.token}",
+        )
+        self.assertEqual(response.status_code, 400)
 
+    def test_get_feed_datial(self):
+        """특정 피드 접근 테스트"""
+
+        # 유저 아파트 설정 전 접근
         response = self.client.get(
             path=self.url + "1/",
             HTTP_AUTHORIZATION=f"token {self.token}",
         )
         self.assertEqual(response.status_code, 403)
 
+        # 유저 아파트 설정
         self.user.my_houses.add(self.apt)
         self.user.save()
+
+        # 본인 아파트의 특정feed에 성공적으로 접근
         response = self.client.get(
             path=self.url + "1/",
             HTTP_AUTHORIZATION=f"token {self.token}",
         )
         self.assertEqual(response.status_code, 200)
 
+        # 본인 아파트의 존재하지 않은 feed에 접근
         response = self.client.get(
             path=self.url + "2/",
             HTTP_AUTHORIZATION=f"token {self.token}",
